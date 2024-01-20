64813458f3b1dfae2a07227adf8dffd77d6b22a2
UmangJagatap <122389993+UmangJagatap@users.noreply.github.com>
2024-01-20 23:14:55 +0530

    React Routing is done

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css'
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <div className="layout">
+      <header className="header">
+        <nav>
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link to={"/dashboard"}>Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="content">
+        <Outlet />
+      </main>
+      <footer className="footer">this is footer</footer>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <h1>This is Dashboard</h1>
+  );
+}
+
+function Home() {
+  return (
+    <h1>This is Home</h1>
+  );
+}
+
+function About() {
+  return (
+    <h1>This is About</h1>
+  );
 }
 
 export default App
