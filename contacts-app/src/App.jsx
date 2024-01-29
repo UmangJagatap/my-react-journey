367d72b2211520e7a74d1a56b3a4ac359cac4a77
UmangJagatap <122389993+UmangJagatap@users.noreply.github.com>
2024-01-29 22:21:07 +0530

    work on adding contacts list and search functionality

--- contacts-app/src/App.jsx
+++ contacts-app/src/App.jsx
@@ -1,33 +1,60 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
+
 import { css } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 import ContactList from './ContactList';
 import './App.css'
+import { useState, useRef } from 'react';
+
+function Header({ onSearch }) {
+
+  const searchElementRef = useRef(null);
+  function onSubmit(e) {
+    e.preventDefault();
+    onSearch(searchElementRef.current.value);
+  }
+
+  return (
+    <header css={css`
+          border-bottom: 1px solid;
+          padding:1rem;
+      `}>
+      <form css={css`
+            display: flex;
+            justify-content: space-between;
+        `} onSubmit={onSubmit}>
+        <input css={css`
+            padding:8px;
+        `}
+          ref={searchElementRef} type="text" />
+        <button>Search</button>
+      </form>
+    </header>
+  );
+}
+
 
 function Sidenav() {
+
+  const [searchText, setSearchText] = useState("");
+  function onSearch(text) {
+    setSearchText(text);
+  }
+
   return (
     <aside css={css`
         border-right: 1px solid;
         display:grid;
         grid-template-rows:auto 1fr auto;
     `}>
-      <header css={css`
-          border-bottom: 1px solid;
-          padding:16px;
-      `}>
-        <form css={css`
-            display: flex;
-            justify-content: space-between;
-            padding: 8px;
-        `}>
-          <input type="text" />
-          <button>Search</button>
-        </form>
-      </header>
+      <Header onSearch={onSearch} />
       <section css={css`
-          padding:16px;
+          padding:1rem;
+          max-height: 40rem;
+          overflow: auto;
       `}>
-        <ContactList />
+        <ContactList searchText={searchText} />
       </section>
       <footer css={css`
           border-top: 1px solid;
--- contacts-app/src/ContactList.jsx
+++ contacts-app/src/ContactList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 // import React from 'react'
 
@@ -6,31 +7,41 @@ import { useLoaderData, NavLink } from "react-router-dom"
 import { css } from "@emotion/react";
 
 
-export default function ContactList() {
-    const contacts = useLoaderData();
+export default function ContactList({ searchText }) {
+    const contacts = useLoaderData() ?? [];
+    const filteredContacts = contacts.filter(contact => {
+        const { first, last } = contact.name;
+        return first.toLowerCase().includes(searchText) || last.toLowerCase().includes(searchText)
+    })
     console.log(contacts);
     return (
-        <ul css={css`
+        <ul css={css`  
             list-style: none;
             padding: 0;
         `}>{
 
-            contacts?.length ? contacts.map((contact) => {
-                let {
-                    id: { value },
-                    name: { first, last },
-                } = contact;
-                return (
-                    <li key={value}>
-                        <NavLink css={css({
-                            display:'block',
-                            padding:'4px',
-                            textDecoration: 'none', 
-                        })} to={`/contacts/${value}`}>{`${first} ${last}`}</NavLink>
-                    </li>
-                )
-            })
-                : null}
+                filteredContacts?.length ? filteredContacts.map((contact) => {
+                    let {
+                        id: { value },
+                        name: { first, last },
+                    } = contact;
+                    return (
+                        <li key={value}>
+                            <NavLink css={css({
+                                display: 'block',
+                                padding: '4px 8px',
+                                textDecoration: 'none',
+                                color: 'royalblue',
+                                "&.active": {
+                                    backgroundColor: 'royalblue',
+                                    color: 'white',
+                                    borderRadius: '8px',
+                                },
+                            })} to={`/contacts/${value}`}>{`${first} ${last}`}</NavLink>
+                        </li>
+                    )
+                })
+                    : null}
         </ul>
     );
 }
